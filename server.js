24dba89a831c0a99d7de0579aeb60009d7a01119
chyppoli <claude.hyppolite@gmail.com>
2020-11-22 11:30:30 -0500

    add functions to handle select queries for employees, departments and titles

---
+++ server.js
@@ -0,0 +1,60 @@
+// const express = require("express");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const asTable = require("as-table");
+
+// const app = express();
+// const PORT = process.env.PORT || 8080;
+
+//  mysql connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Toofy471",
+  database: "employees_db",
+});
+
+// initiate MySQL connection
+connection.connect((err) => {
+  if (err) {
+    console.log("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id: " + connection.threadId);
+  //   queryAllEmployees();
+  queryAllDepartments();
+  queryAllTitles();
+});
+
+// function to view all employees
+function queryAllEmployees() {
+  const sqlAllEmp = "SELECT * FROM V_VIEW_EMPLOYEES;";
+  connection.query(sqlAllEmp, (err, res) => {
+    if (err) throw err;
+    console.table(asTable(res));
+  });
+}
+
+// function to view all departments
+function queryAllDepartments() {
+  const qryAllDepartments = "Select * from tblDepartment;";
+  connection.query(qryAllDepartments, (err, res) => {
+    if (err) throw err;
+    console.table(asTable(res));
+  });
+}
+
+// function to view all Titles
+function queryAllTitles() {
+  const qryAllTitles = "Select * from tblRole;";
+  connection.query(qryAllTitles, (err, res) => {
+    if (err) throw err;
+    console.table(asTable(res));
+  });
+}
+
+//function add new roles
+// function sqlInsertRole() {
+//     strSql = 
+// }
